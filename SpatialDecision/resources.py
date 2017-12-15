922ddd4e08b9454c59a5ffe1f9c002f5f4dc55ac
TenguWu <33900474+TenguWu@users.noreply.github.com>
2017-12-15 09:28:16 +0100

    new plugin test
    new plugin test

--- SpatialDecision/resources.py
+++ SpatialDecision/resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
+# Created: 周二 12月 5 16:10:26 2017
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
